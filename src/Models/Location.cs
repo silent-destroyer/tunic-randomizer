8b6db2549da2f41e9172c50adb21c85bc647d4b6
jimtheeternal <avri@rahamim.com>
2023-05-02 03:07:58 -0700

    Logic works! Hints dont...

--- src/Models/Location.cs
+++ src/Models/Location.cs
@@ -19,7 +19,7 @@ namespace TunicRandomizer {
             get;
             set;
         }
-        public List<string> RequiredItems {
+        public List<Dictionary<string, int>> RequiredItems {
             get;
             set;
         }
@@ -48,12 +48,50 @@ namespace TunicRandomizer {
             Position = position;
         }
         
-        public Location(string locationId, string sceneName, int sceneId, string position, List<string> requiredItems) {
+        public Location(string locationId, string sceneName, int sceneId, string position, List<Dictionary<string, int>> requiredItems) {
             LocationId = locationId;
             SceneName = sceneName;
             SceneId = sceneId;
             Position = position;
             RequiredItems = requiredItems;
+        }
+
+        public bool reachable(Dictionary<string, int> inventory) {
+            //if there are no requirements, the location is reachable
+            if (this.RequiredItems.Count == 0) {
+                return true;
+            }
+
+            //if there are requirements, loop through each requirement to see if any are fully met
+            foreach (Dictionary<string, int> req in this.RequiredItems) {
+                //ensure req and items use same terms
+                if (SaveFile.GetInt("randomizer sword progression enabled") != 0) {
+                    if (req.ContainsKey("Stick")) {
+                        req["Sword Progression"] = 1;
+                        req.Remove("Stick");
+                    }
+                    if (req.ContainsKey("Sword")) {
+                        req["Sword Progression"] = 2;
+                        req.Remove("Sword");
+                    }
+                }
+
+                //check if this requirement is fully met, otherwise move to the next requirement
+                int met = 0;
+                foreach (string item in req.Keys) {
+                    if (!inventory.ContainsKey(item)) {
+                        break;
+                    } else if(inventory[item] >= req[item]) {
+                        met += 1;
+                    }
+                }
+                if (met == req.Count) {
+                    return true;
+                }
+            }
+
+            //if no requirements are met, the location isn't reachable
+            return false;
         }
     }
 }
--- src/PluginInfo.cs
+++ src/PluginInfo.cs
@@ -1,7 +1,7 @@
 ﻿namespace TunicRandomizer {
     public class PluginInfo {
         public const string NAME = "Tunic Randomizer";
-        public const string VERSION = "0.0.10";
+        public const string VERSION = "0.0.101";
         public const string GUID = "com.silentdestroyer.tunicrandomizer";
     }
 }
