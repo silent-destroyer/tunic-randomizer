e97d19177256365c90dfa3735527f72f52403f99
Scipio Wright <scipiowright@gmail.com>
2024-03-22 10:24:07 -0400

    Add DestinationTag and DestinationSceneTag, refactor slightly

--- src/Data/Portal.cs
+++ src/Data/Portal.cs
@@ -3,18 +3,24 @@
 namespace TunicRandomizer {
     public class Portal {
         //private static ManualLogSource Logger = TunicRandomizer.Logger;
-        public string Scene { get; set; }
-        public string Destination { get; set; }
         public string Name { get; set; }
+        public string Destination { get; set; }
+        public string Tag { get; set; }
+        public string Scene { get; set; }
         public string Region { get; set; }
+        public string DestinationTag { get; set; }
         public string SceneDestinationTag { get; set; }
+        public string DestinationSceneTag { get; set; }
 
-        public Portal(string name, string destination, string scene, string region) {
+        public Portal(string name, string destination, string tag, string scene, string region) {
             Name = name;
             Destination = destination;
+            Tag = tag;
             Scene = scene;
             Region = region;
-            SceneDestinationTag = (Scene + ", " + Destination);
+            DestinationTag = (Destination + Tag);
+            SceneDestinationTag = (Scene + ", " + DestinationTag);
+            DestinationSceneTag = (Destination + ", " + Scene + Tag);  // for finding the vanilla connection
         }
 
         public bool CanReach(Dictionary<string, int> inventory) {
