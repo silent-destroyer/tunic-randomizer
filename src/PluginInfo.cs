d211fa06ab9d583080fba0c0b28d17fae6c65d40
Scipio Wright <scipiowright@gmail.com>
2024-04-06 19:32:16 -0400

    increment version number

--- src/PluginInfo.cs
+++ src/PluginInfo.cs
@@ -1,7 +1,7 @@
 ﻿namespace TunicRandomizer {
     public class PluginInfo {
         public const string NAME = "Tunic Randomizer";
-        public const string VERSION = "3.1.1";
+        public const string VERSION = "3.1.2";
         public const string GUID = "silentdestroyer.tunic.randomizer";
     }
 }
