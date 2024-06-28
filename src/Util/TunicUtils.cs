5f92aa514ba602dd9a168188407aea5cf9405791
Scipio Wright <scipiowright@gmail.com>
2024-06-28 14:18:13 -0400

    Update TunicUtils.cs

--- src/Util/TunicUtils.cs
+++ src/Util/TunicUtils.cs
@@ -8,10 +8,10 @@ namespace TunicRandomizer {
     public class TunicUtils {
         // list of the CheckIds for checks that are currently in logic
         public static List<string> ChecksInLogic = new List<string>();
-        // progression items the player has received
+        // items the player has received
         public static Dictionary<string, int> PlayerItemsAndRegions = new Dictionary<string, int>();
 
-        // add a key if it doesn't exist, otherwise increment the value by 1
+        // add keys if they don't exist, otherwise increment their values by 1
         public static Dictionary<string, int> AddListToDict(Dictionary<string, int> dictionary, List<string> list) {
             foreach (string item in list) {
                 dictionary.TryGetValue(item, out var count);
@@ -20,16 +20,18 @@ namespace TunicRandomizer {
             return dictionary;
         }
 
+        // add a key if it doesn't exist, otherwise increment the value by 1
         public static Dictionary<string, int> AddStringToDict(Dictionary<string, int> dictionary, string item) {
             dictionary.TryGetValue(item, out var count);
             dictionary[item] = count + 1;
             return dictionary;
         }
 
-        public static Dictionary<string, int> AddDictToDict(Dictionary<string, int> dictionary1, Dictionary<string, int> dictionary2) {
-            foreach (KeyValuePair<string, int> pair in dictionary2) {
-                dictionary1.TryGetValue(pair.Key, out var count);
-                dictionary1[pair.Key] = count + pair.Value;
+        // for combining dictionaries of item quantities
+        public static Dictionary<string, int> AddDictToDict(Dictionary<string, int> primaryDictionary, Dictionary<string, int> secondaryDictionary) {
+            foreach (KeyValuePair<string, int> pair in secondaryDictionary) {
+                primaryDictionary.TryGetValue(pair.Key, out var count);
+                primaryDictionary[pair.Key] = count + pair.Value;
             }
             return dictionary1;
         }
@@ -39,11 +41,12 @@ namespace TunicRandomizer {
             PlayerItemsAndRegions.Clear();
             ChecksInLogic.Clear();
 
-            TunicUtils.AddListToDict(PlayerItemsAndRegions, ItemRandomizer.PrecollectedItems);
+            AddListToDict(PlayerItemsAndRegions, ItemRandomizer.PrecollectedItems);
             PlayerItemsAndRegions.Add("Overworld", 1);
 
+            // archipelago and standalone have different methods to state which items have been received
             if (SaveFlags.IsArchipelago()) {
-                TunicUtils.AddDictToDict(PlayerItemsAndRegions, Archipelago.instance.integration.GetStartInventory());
+                AddDictToDict(PlayerItemsAndRegions, Archipelago.instance.integration.GetStartInventory());
                 foreach (var itemInfo in Archipelago.instance.integration.session.Items.AllItemsReceived) {
                     string itemName = itemInfo.ItemName;
                     // convert display name to internal name
@@ -52,18 +55,20 @@ namespace TunicRandomizer {
                             itemName = namePair.Key;
                         }
                     }
-                    TunicUtils.AddStringToDict(PlayerItemsAndRegions, itemName);
+                    AddStringToDict(PlayerItemsAndRegions, itemName);
                 }
             } else {
                 foreach (Check locationCheck in Locations.RandomizedLocations.Values) {
                     if (Locations.CheckedLocations.ContainsKey(locationCheck.CheckId) && Locations.CheckedLocations[locationCheck.CheckId] == true) {
-                        TunicUtils.AddStringToDict(PlayerItemsAndRegions, locationCheck.Reward.Name);
+                        AddStringToDict(PlayerItemsAndRegions, locationCheck.Reward.Name);
                     }
                 }
             }
             UpdateChecksInLogic();
         }
 
+
+        // updates PlayerItemsAndRegions based on which items the player has received, then updates ChecksInLogic based on the player's items/accessible regions
         public static void UpdateChecksInLogic() {
             ItemRandomizer.GetReachableRegions(PlayerItemsAndRegions);
             foreach (Check check in Locations.VanillaLocations.Values) {
