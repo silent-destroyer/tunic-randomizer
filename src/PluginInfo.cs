fa446f64fe03f35ccbc7ef3ef79437aad51f685e
silent-destroyer <osilentdestroyer@gmail.com>
2024-07-09 20:02:31 -0400

    Bump versions

--- src/PluginInfo.cs
+++ src/PluginInfo.cs
@@ -1,7 +1,7 @@
 ﻿namespace TunicRandomizer {
     public class PluginInfo {
         public const string NAME = "Tunic Randomizer";
-        public const string VERSION = "3.2.3";
+        public const string VERSION = "3.2.4";
         public const string GUID = "silentdestroyer.tunic.randomizer";
     }
 }
