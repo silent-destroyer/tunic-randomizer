d6814022e2f9112d942806e627f8ba0862cc87f4
Scipio Wright <scipiowright@gmail.com>
2024-07-28 18:09:00 -0400

    Make shop orientation match entrance orientation (#65)
    Co-authored-by: Silent <110704408+silent-destroyer@users.noreply.github.com>

--- src/Data/Portal.cs
+++ src/Data/Portal.cs
@@ -10,6 +10,7 @@ namespace TunicRandomizer {
         public string DestinationTag { get; set; }
         public string SceneDestinationTag { get; set; }
         public string DestinationSceneTag { get; set; }
+        public int Direction { get; set; }
 
         public Portal(string name, string destination, string tag, string scene, string region) {
             Name = name;
@@ -20,6 +21,18 @@ namespace TunicRandomizer {
             DestinationTag = (Destination + Tag);
             SceneDestinationTag = (Scene + ", " + DestinationTag);
             DestinationSceneTag = (Destination + ", " + Scene + Tag);  // for finding the vanilla connection
+        }
+
+        public Portal(string name, string destination, string tag, string scene, string region, int direction) {
+            Name = name;
+            Destination = destination;
+            Tag = tag;
+            Scene = scene;
+            Region = region;
+            DestinationTag = (Destination + Tag);
+            SceneDestinationTag = (Scene + ", " + DestinationTag);
+            DestinationSceneTag = (Destination + ", " + Scene + Tag);
+            Direction = direction;
         }
 
         public bool CanReach(Dictionary<string, int> inventory) {
