622dfaf19d473183b4f92bfded8d6d20f7e99374
Scipio Wright <scipiowright@gmail.com>
2024-11-29 22:09:17 -0500

    More progress on breakables

--- src/Patches/BreakableShuffle.cs
+++ src/Patches/BreakableShuffle.cs
@@ -1,12 +1,7 @@
-﻿using Archipelago.MultiClient.Net.Models;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using System.Collections.Generic;
-using System.Diagnostics.Eventing.Reader;
 using System.IO;
 using System.Reflection;
-using System.Threading.Tasks;
-using UnityEngine;
-using UnityEngine.SceneManagement;
 
 namespace TunicRandomizer {
     public class BreakableShuffle {
@@ -14,7 +9,23 @@ namespace TunicRandomizer {
         public static Dictionary<string, Check> BreakableChecks = new Dictionary<string, Check>();
         public static Dictionary<string, int> BreakableChecksPerScene = new Dictionary<string, int>();
 
+        // keys are the auto-genned name, values are the nicer name (or the key if the key is already a nice name)
+        public static Dictionary<string, string> BreakableBetterNames = new Dictionary<string, string>() {
+            { "Urn", "Pot" },
+            { "Urn Firey", "Fire Pot" },
+            { "Urn Explosive", "Explosive Pot" },
+            { "Physical Post", "Sign" },
+            { "sewer_barrel", "Barrel" },
+            { "crate", "Crate" },
+            { "crate quarry", "Crate" },
+            { "table", "Table" },
+            { "library_lab_pageBottle", "Library Glass" },  // probably rename this one
+            { "Leaf Pile", "Leaf Pile" },  // actually go find this one
+        };
+
         public static void LoadBreakableChecks() {
+            System.Random random = new System.Random(SaveFile.GetInt("seed"));
+            List<int> moneyAmounts = new List<int> { 1, 1, 1, 5, 5, 10 };
             var assembly = Assembly.GetExecutingAssembly();
             var breakableJson = "TunicRandomizer.src.Data.Breakables.json";
             //List<string> breakers = new List<string>() { "Stick", "Sword", "Techbow", "Gun" };
@@ -36,15 +47,15 @@ namespace TunicRandomizer {
                         foreach (KeyValuePair<string, List<string>> breakableGroup in regionGroup.Value) {
                             string breakableName = breakableGroup.Key;
                             foreach (string breakablePosition in breakableGroup.Value) {
-                                breakableName = $"{sceneName}~{regionName}~{breakableName}~{breakablePosition}";
+                                string breakableFullName = $"{regionName}~{breakableName}~{breakablePosition}";  // also used for checkId, and checkId has scene name in it already
                                 Check check = new Check(new Reward(), new Location());
-                                check.Reward.Name = "Small Money";  // change later
-                                check.Reward.Type = "idk";
-                                check.Reward.Amount = 1;
+                                check.Reward.Name = "money";
+                                check.Reward.Type = "MONEY";
+                                check.Reward.Amount = moneyAmounts[random.Next(moneyAmounts.Count)];  // todo: maybe change later
 
                                 check.Location.SceneName = sceneName;
                                 check.Location.SceneId = 0;  // Update this if sceneid ever actually gets used for anything
-                                check.Location.LocationId = breakableName;
+                                check.Location.LocationId = breakableFullName;
                                 check.Location.Position = breakablePosition;
                                 check.Location.Requirements = new List<Dictionary<string, int>>();
                                 // check extra reqs
@@ -53,13 +64,14 @@ namespace TunicRandomizer {
                                 foreach (string breaker in breakers) {
                                     check.Location.Requirements.Add(new Dictionary<string, int> { { breaker, 1 }, { regionName, 1 } });
                                 }
+                                // todo: consider having actual location names instead of just coordinates
+                                string description = $"{Locations.SimplifiedSceneNames[sceneName]} - {BreakableBetterNames[breakableName]} ({breakablePosition})";
+                                
                             }
                         }
                     }
                 }
-
             }
-
             
         }
     }
