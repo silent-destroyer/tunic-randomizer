8cf18dbe21279d6137275d9715387fb58b76fffe
Silent <osilentdestroyer@gmail.com>
2025-03-20 20:12:00 -0400

    Add version info

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using TunicRandomizer;
+
+[assembly: AssemblyTitle("TUNIC Randomizer")]
+[assembly: AssemblyDescription("A randomizer for TUNIC")]
+[assembly: AssemblyProduct("TUNIC Randomizer")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion(PluginInfo.VERSION)]
