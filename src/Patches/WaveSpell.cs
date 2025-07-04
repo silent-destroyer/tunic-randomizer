9d7f039bb0fed4a7a99e50dbf8a7a0b63a573438
Scipio Wright <scipiowright@gmail.com>
2025-07-03 21:50:50 -0400

    Make wave spell work with alternate interpretations (#125)
    * Make wave spell work with alternate interpretations
    
    * Add comments
    
    * Fix alt spells
    
    ---------
    
    Co-authored-by: Silent <osilentdestroyer@gmail.com>

--- src/Patches/WaveSpell.cs
+++ src/Patches/WaveSpell.cs
@@ -7,6 +7,8 @@ namespace TunicRandomizer {
     public class WaveSpell : HealSpell {
 
         public static List<DPAD> CustomInputs = new List<DPAD>() { };
+        public static List<DPAD> CustomInputsAlt = new List<DPAD>() { };
+        public static List<DPAD> CustomInputsAltAlt = new List<DPAD>() { };
 
         public WaveSpell(IntPtr ptr) : base(ptr) { }
 
@@ -16,21 +18,52 @@ namespace TunicRandomizer {
             base.hpToGive = 0;
             base.particles = PlayerCharacter.instance.transform.GetChild(8).gameObject.GetComponent<ParticleSystem>();
             List<DPAD> dPADs = new List<DPAD>() { DPAD.UP, DPAD.RIGHT, DPAD.LEFT, DPAD.DOWN };
+            // read left to right across the screen
             List<int> inputs = new List<int>() { 2, 3, 1, 3, 2, 2, 3, 1, 0, 1, 1, 3, 1, 1, 0, 2, 2, 2, 0, 1, 0, 1, 1, 3, 3, 2, 0, 0, 2, 3 };
+            // read left to right on the left side of the screen first then the right side of the screen
+            List<int> inputsAlt = new List<int>() { 2, 3, 1, 3, 1, 0, 1, 1, 0, 2, 0, 1, 0, 1, 1, 0, 0, 2, 3, 2, 2, 1, 3, 1, 2, 2, 3, 3, 2, 3 };
+            // read left to right column by column, the time found one is the last input
+            List<int> inputsAltAlt = new List<int>() { 2, 3, 3, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 1, 2, 1, 1, 2, 3, 1, 3, 2, 3, 3, 2, 2, 1, 2, 2, 3 };
             CustomInputs.Clear();
+            CustomInputsAlt.Clear();
+            CustomInputsAltAlt.Clear();
             for (int i = 0; i < inputs.Count; i++) {
                 CustomInputs.Add(dPADs[inputs[i]]);
+                CustomInputsAlt.Add(dPADs[inputsAlt[i]]);
+                CustomInputsAltAlt.Add(dPADs[inputsAltAlt[i]]);
             }
         }
 
         public override bool CheckInput(Il2CppStructArray<DPAD> inputs, int length) {
             if (length == CustomInputs.Count) {
+                bool success = true;
                 for (int i = 0; i < length; i++) {
-                    if (inputs[i] != CustomInputs[i]) {
-                        return false;
+                    if (inputs[i] != CustomInputsAlt[i]) {
+                        success = false;
+                        break;
                     }
                 }
-                DoWave();
+                if (!success) {
+                    success = true;
+                    for (int i = 0; i < length; i++) {
+                        if (inputs[i] != CustomInputs[i]) {
+                            success = false;
+                            break;
+                        }
+                    }
+                }
+                if (!success) {
+                    success = true;
+                    for (int i = 0; i < length; i++) {
+                        if (inputs[i] != CustomInputsAltAlt[i]) {
+                            success = false;
+                            break;
+                        }
+                    }
+                }
+                if (success) {
+                    DoWave();
+                }
             }
             return false;
         }
